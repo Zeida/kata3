e04ff2a11f90683a11d8814f03c2385eaa8c9dbb
45395507 <45395507@DESKTOP-3D5M5VE>
2019-01-16 21:44:47 +0000

    kata3 Version2.1

--- src/kata3/HistogramDisplay.java
+++ src/kata3/HistogramDisplay.java
@@ -1,8 +1,7 @@
 package kata3;
 
-
-import java.awt.Container;
 import java.awt.Dimension;
+import javax.swing.JPanel;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -10,58 +9,51 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.ui.ApplicationFrame;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+public class HistogramDisplay extends ApplicationFrame{
 
-/**
- *
- * @author 45395507
- */
-public class HistogramDisplay extends ApplicationFrame {
-    
-    public HistogramDisplay() {
+    private final Histogram<String> histogram;
+
+    public HistogramDisplay(Histogram<String> histogram) {
         super("Histograma");
+
+        this.histogram = histogram;
         setContentPane(createPanel());
         pack();
     }
-    
+
     public void execute(){
         setVisible(true);
     }
-    
 
-    private Container createPanel() {
-        ChartPanel chartPanel= new ChartPanel(createChart(createDataset()));
+    private JPanel createPanel(){
+        ChartPanel chartPanel = new ChartPanel(createChart(createDataset()));
         chartPanel.setPreferredSize(new Dimension(500,400));
         return chartPanel;
     }
-    
-    private JFreeChart createChart(DefaultCategoryDataset dataSet){
-        JFreeChart chart;
-        chart = ChartFactory.createBarChart("JFreeChart", 
-                "Dominios email",
-                "Nº email",
-                dataSet,
-                PlotOrientation.VERTICAL,
-                false,
-                true,
-                rootPaneCheckingEnabled);
 
-            return chart;
+    private JFreeChart createChart (DefaultCategoryDataset dataSet){
+
+        JFreeChart chart = 
+                    ChartFactory.createBarChart("JFreeChart", 
+                                                "Dominios email", 
+                                                "Nº email", 
+                                                dataSet, 
+                                                PlotOrientation.VERTICAL, 
+                                                false, 
+                                                true, 
+                                                rootPaneCheckingEnabled);
+
+        return chart;
     }
-    
-        private DefaultCategoryDataset createDataset(){
 
-            DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
-            dataSet.addValue(15,"","ulpgc.es");
-            dataSet.addValue(8,"","ull.es");
-            dataSet.addValue(2,"","hotmail.com");
-            dataSet.addValue(6,"","gmail.com");
+    private DefaultCategoryDataset createDataset(){
 
-            return dataSet;
-        }
-    
+        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+
+        for(String key: histogram.keySet()) dataSet.addValue(histogram.get(key),
+                                                                "", key);
+
+        return dataSet;
+    }
+
 }
--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -1,23 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kata3;
 
-/**
- *
- * @author 45395507
- */
 public class Kata3 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        HistogramDisplay histo = new HistogramDisplay();
+
+        Histogram<String> histogram = new Histogram<>();
+
+        for(int i = 0; i <= 15; i++) histogram.increment("ulpgc.es");
+
+        for(int i = 0; i <= 8; i++) histogram.increment("ull.es");
+
+        for(int i = 0; i <= 2; i++) histogram.increment("hotmail.com");
+
+        for(int i = 0; i <= 6; i++) histogram.increment("gmail.com");
+
+        HistogramDisplay histo = new HistogramDisplay(histogram);
         histo.execute();
-    }
-    
+    }    
 }
